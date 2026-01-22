903ea8070422951ad880dd93229ee80a576d8244
Derinola-code <olaleyeaderinola@gmail.com>
2026-01-22 14:30:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,9 +27,8 @@ def index():
 
     return render_template("index.html", prediction=prediction)
 
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
